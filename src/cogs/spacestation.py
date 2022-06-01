e001fc9282fababa956fa44edfa69ca6ad97911c
yo56789 <71994752+yo56789@users.noreply.github.com>
2022-06-01 19:21:14 -0400

    Update spacestation.py

--- src/cogs/spacestation.py
+++ src/cogs/spacestation.py
@@ -12,18 +12,18 @@ class SpaceStation(commands.Cog):
         print('init run')
         self.bot = bot
 
-    @slash_command(guild_ids=[801646969676234782])
+    @slash_command()
     @option("level", type=int, description="View the level of a lab module", default=1, min_value=1, max_value=5)
     async def moduleinfo(self, ctx: discord.ApplicationContext, level: str):
         labmod = LabModule(int(level))
         await ctx.respond(embed=labmod.as_embed())
 
-    @slash_command(guild_ids=[801646969676234782])
+    @slash_command()
     # @option(name="tests", type=str, description="This commands is testing this exact thing", default="a")
     async def test(self, ctx: discord.ApplicationContext, tests: discord.Option(str, autocomplete=discord.utils.basic_autocomplete(["test1", "test3"])), testt: discord.Option(str, choices=["test1", "test3"])):
         await ctx.respond(f"{tests} hm")
 
-    @slash_command(guild_ids=[801646969676234782])
+    @slash_command()
     async def create_account(self, ctx: discord.ApplicationContext):
         await Users.create(userid=ctx.user.id)
         await ctx.respond("Created account (check db)")
